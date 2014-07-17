e3cc2f46012294ee67b26ff50f1dde5a7cc6a650
ac_airbear <acolby@live.com>
2014-07-16 21:10:10 -0700

    added scss

--- public/scripts/controllers/Home.js
+++ public/scripts/controllers/Home.js
@@ -75,6 +75,7 @@ angular.module('FriendZone')
 
 			// Suport functions
 			// Gets the color of a username through our hash function
+
 			var COLORS = [
 				'#e21400', '#91580f', '#f8a700', '#f78b00',
 				'#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
@@ -84,7 +85,7 @@ angular.module('FriendZone')
 				// Compute hash code
 				var hash = 7;
 				for (var i = 0; i < username.length; i++) {
-					hash = username.charCodeAt(i) + (hash << 5) - hash;
+					hash = username.charCodeAt(i) + (hash < 5) - hash;
 				}
 				// Calculate color
 				var index = Math.abs(hash % COLORS.length);
